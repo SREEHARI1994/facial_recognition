db0ba741c8d674eb12c9fa7c66adcaef6384035f
SREEHARI1994 <sreehari.mras@gmail.com>
2025-11-06 02:04:52 +0530

    versiom 0.0.6 python package added here

--- simpleface/add_faces.py
+++
@@ -1,4 +0,0 @@
-from .recognizer import SimpleFaceRecognizer
-r = SimpleFaceRecognizer()
-r.add_person("Sruthi", "simpleface/faces/sruthi.jpg")
-r.add_person("Sreehari", "simpleface/faces/sreehari.jpg")
